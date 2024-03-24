606089451ea46765d57903366890556b6c854e82
Konrad Czardybon <konradcz2001@gmail.com>
2024-03-24 13:57:03 +0100

    Add project files.

---
+++ Zadanie1Singleton/Logger.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Logger
+{
+    private static Logger instance;
+    private List<string> logMessages;
+
+    private Logger()
+    {
+        logMessages = new List<string>();
+    }
+
+    public static Logger GetInstance()
+    {
+        if (instance == null)
+            instance = new Logger();
+
+            return instance;
+    }
+
+    public void LogMessage(string message)
+    {
+        logMessages.Add($"{DateTime.Now}: {message}");
+    }
+
+    public void DisplayLog()
+    {
+        Console.WriteLine("\nLog messages:");
+        foreach (var message in logMessages)
+        {
+            Console.WriteLine(message);
+        }
+        if(logMessages.Count == 0)
+            Console.WriteLine(" -- brak -- ");
+    }
+}
---
+++ Zadanie1Singleton/Program.cs
@@ -0,0 +1,38 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        bool continueLoop = true;
+        Logger logger = Logger.GetInstance();
+
+        while (continueLoop)
+        {
+            DisplayMenu();
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    Console.Write("\nPodaj treść: ");
+                    logger.LogMessage(Console.ReadLine());
+                    break;
+                case "2":
+                    logger.DisplayLog();
+                    break;
+                case "3":
+                    continueLoop = false;
+                    break;
+                default:
+                    Console.WriteLine("\nZły numer!");
+                    break;
+            }
+        }
+    }
+
+    private static void DisplayMenu()
+    {
+        Console.WriteLine("\n1. Nowy log");
+        Console.WriteLine("2. Wyświetl wszystkie logi");
+        Console.WriteLine("3. Zakończ program");
+        Console.Write("Wybierz numer: ");
+    }
+}
